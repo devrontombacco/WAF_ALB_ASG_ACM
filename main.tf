dde09744fef2e5ad39d599ac3362dc9d42e2469f
Devron Tombacco <tombacco.devron@gmail.com>
2025-05-15 19:38:03 +0100

    add EC2 (x2) instances

--- main.tf
+++ main.tf
@@ -75,3 +75,66 @@ resource "aws_route" "internet_access_route" {
   gateway_id             = aws_internet_gateway.igw.id
 
 }
+
+# Dynamically create Ubuntu AMI for EC2
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# Create EC2 instance in subnet 1a
+resource "aws_instance" "ec2_instance_1a" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  availability_zone = "eu-west-1a"
+  subnet_id         = aws_subnet.public_subnet1a.id
+  key_name          = "MY_EC2_INSTANCE_KEYPAIR"
+
+  tags = {
+    Name = "ec2_instance_1a"
+  }
+  #vpc_security_group_ids = [aws_security_group.public_ec2_sg.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  yes | sudo apt update 
+  yes | sudo apt install apache2
+  echo "<h1>Server Details</h1><p><strong>Hostname:</strong> $(hostname)</p><p><strong>IP Address:</strong>$(hostname -I | cut -d" " -f1)</strong></p>"> /var/www/html/index.html
+  sudo systemctl restart apache2
+  EOF 
+
+}
+
+# Create EC2 instance in subnet 1b
+resource "aws_instance" "ec2_instance_1b" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  availability_zone = "eu-west-1b"
+  subnet_id         = aws_subnet.public_subnet1b.id
+  key_name          = "MY_EC2_INSTANCE_KEYPAIR"
+
+  tags = {
+    Name = "ec2_instance_1b"
+  }
+  #vpc_security_group_ids = [aws_security_group.public_ec2_sg.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  yes | sudo apt update 
+  yes | sudo apt install apache2
+  echo "<h1>Server Details</h1><p><strong>Hostname:</strong> $(hostname)</p><p><strong>IP Address:</strong>$(hostname -I | cut -d" " -f1)</strong></p>"> /var/www/html/index.html
+  sudo systemctl restart apache2
+  EOF 
+
+}
