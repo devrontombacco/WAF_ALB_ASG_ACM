3bd3d978a8d761b0539a369d31ba0613ee773555
Devron Tombacco <tombacco.devron@gmail.com>
2025-05-15 19:18:18 +0100

    Add main_vpc

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 
 
 # Create a VPC
-# resource "aws_vpc" "example" {
-#   cidr_block = "10.0.0.0/16"
-# }
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "12.0.0.0/16"
+
+   tags = {
+    Name = "main_vpc"
+  }
+}
 
