c5a472b49acde91d25fa4596b7477c04af77b0bc
Devron Tombacco <tombacco.devron@gmail.com>
2025-05-17 19:10:45 +0100

    correct error with security groups

--- main.tf
+++ main.tf
@@ -176,3 +176,34 @@ resource "aws_security_group" "ec2-sg-ssh-http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Create Target Group
+
+resource "aws_lb_target_group" "alb-tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.main_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+}
+
+# Register EC2 instances in TG 
+resource "aws_lb_target_group_attachment" "alb_tg_attachment_1a" {
+  target_group_arn = aws_lb_target_group.alb-tg.arn
+  target_id        = aws_instance.ec2_instance_1a.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "alb_tg_attachment_1b" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.ec2_instance_1b.id
+  port             = 80
+}
