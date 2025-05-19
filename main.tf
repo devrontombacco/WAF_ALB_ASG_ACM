735fc76e8acede3369778f0f3a189e91dec72adc
Devron Tombacco <tombacco.devron@gmail.com>
2025-05-19 18:17:50 +0100

    Add ALB + Listener

--- main.tf
+++ main.tf
@@ -232,3 +232,28 @@ resource "aws_lb_target_group_attachment" "alb_tg_attachment_1b" {
   port             = 80
 }
 
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg_ssh.id]
+  subnets            = [
+    aws_subnet.public_subnet1a.id,
+    aws_subnet.public_subnet1b.id
+  ]
+
+  tags = {
+    Name = "app-alb"
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-tg.arn
+  }
+}
