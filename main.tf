fa6389a3ce118bd6ad10fa1b4986aaf5746f4aea
Devron Tombacco <tombacco.devron@gmail.com>
2025-05-25 17:20:36 +0100

    put TG creation, ALB creationg and ALB's Listener creation just after SLB's SG creation

--- main.tf
+++ main.tf
@@ -114,7 +114,64 @@ resource "aws_security_group" "alb_sg_http_https_ssh" {
   }
 }
 
+# Create Target Group
 
+resource "aws_lb_target_group" "alb-tg" {
+  name     = "alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg_http_https_ssh.id]
+  subnets            = [
+    aws_subnet.public_subnet1a.id,
+    aws_subnet.public_subnet1b.id
+  ]
+
+  tags = {
+    Name = "app-alb"
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-tg.arn
+  }
+}
+
+# Create Launch Template for Auto-Scaling Group
+resource "aws_launch_template" "launch_template_for_asg" {
+  name_prefix   = "my-template-"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+
+  network_interfaces {
+    security_groups = [aws_security_group.ec2-sg.id]
+    associate_public_ip_address = true
+  }
+
+  user_data = base64encode(file("user_data.sh"))
+}
 
 
 
@@ -188,64 +245,7 @@ resource "aws_security_group" "ec2-sg" {
 
 
 
-# Create Target Group
 
-resource "aws_lb_target_group" "alb-tg" {
-  name     = "alb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main_vpc.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb" "app_alb" {
-  name               = "app-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg_http_https_ssh.id]
-  subnets            = [
-    aws_subnet.public_subnet1a.id,
-    aws_subnet.public_subnet1b.id
-  ]
-
-  tags = {
-    Name = "app-alb"
-  }
-}
-
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb-tg.arn
-  }
-}
-
-# Create Launch Template for Auto-Scaling Group
-resource "aws_launch_template" "launch_template_for_asg" {
-  name_prefix   = "my-template-"
-  image_id      = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name      = var.key_name
-
-  network_interfaces {
-    security_groups = [aws_security_group.ec2-sg.id]
-    associate_public_ip_address = true
-  }
-
-  user_data = base64encode(file("user_data.sh"))
-}
 
 # Create Auto-Scaling Group 
 resource "aws_autoscaling_group" "asg_for_main_vpc" {
