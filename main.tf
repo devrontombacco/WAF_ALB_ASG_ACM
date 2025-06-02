2b8f6ac93d429163a5d593cabc467bed24f68e79
Devron Tombacco <tombacco.devron@gmail.com>
2025-06-02 17:30:38 +0100

    Leave comments in main.tf file to act as index

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
 
+# Main Terraform file - High-Level Overview
+# -----------------------------
+#
+# Provider configuration is in provider.tf
+# Variables are declared in variables.tf and populated in terraform.tfvars
+# Networking resources are in networking.tf
+# Security groups are in security.tf
+# EC2 & Auto Scaling Group are in compute.tf
+# Load Balancer and WAF are in loadbalancer.tf
+# Outputs are in outputs.tf
